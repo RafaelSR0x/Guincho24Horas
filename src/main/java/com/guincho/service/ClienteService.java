79d7e447d0c09b2554816aa28073b76f9a8550c7
RafaelSR0x <raffssantana007@gmail.com>
2025-08-29 02:48:30 -0300

    ClienteService to make business rules

--- src/main/java/com/guincho/service/ClienteService.java
+++ src/main/java/com/guincho/service/ClienteService.java
@@ -1,4 +1,28 @@
 package com.guincho.service;
 
+import com.guincho.dto.ClienteDTO;
+import com.guincho.entities.Cliente;
+import com.guincho.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
 public class ClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @Transactional
+    public Cliente create(Cliente cliente) {
+
+        return clienteRepository.save((cliente));
+    }
+
+    @Transactional()
+    public List<ClienteDTO> findAll() {
+        return clienteRepository.findAll().stream().map(ClienteDTO::new).toList();
+    }
 }
